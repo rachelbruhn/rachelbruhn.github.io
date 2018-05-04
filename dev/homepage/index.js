4866232ac9171e87066092cd45f29cc7bf319827
Jordan O'Leary <jdoleary@gmail.com>
2018-05-04 18:48:54 -0400

    feat (Icons) Icon color changes based on current section.  Youtube controls

--- dev/homepage/index.js
+++ dev/homepage/index.js
@@ -6,6 +6,14 @@ $(()=>{
         if(content){
             $('.content').removeClass('visible');
             $(`.content--${content}`).addClass('visible');
+            
+            // Manage icon color based on which content type is selected:
+            const colorIcons = $(this).data('color-icons').split(',');
+            $(`.icon`).removeClass('icon--color');
+            for(let i = 0; i < colorIcons.length; i++){
+                const iconName = colorIcons[i];
+                $(`.icon.icon--${iconName}`).addClass('icon--color');
+            }
         }
     });
 });
