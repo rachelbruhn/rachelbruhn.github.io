4d0d4032d40666f9939980d3f589fb4da7780e22
Jordan O'Leary <jdoleary@gmail.com>
2018-06-03 08:23:24 -0400

    build

--- homepage.2d6272fe.js
+++ homepage.2d6272fe.js
@@ -77,7 +77,7 @@ parcelRequire = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({47:[function(require,module,exports) {
+})({27:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -107,7 +107,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],38:[function(require,module,exports) {
+},{}],24:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -138,35 +138,55 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":47}],27:[function(require,module,exports) {
+},{"./bundle-url":27}],23:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./..\\assets\\background-texture.jpg":[["background-texture.1c258b3f.jpg",39],39],"./..\\assets\\Images\\design.png":[["design.022f4afc.png",40],40],"./..\\assets\\Images\\videos-bw.png":[["videos-bw.b277fe83.png",43],43],"./..\\assets\\Images\\videos.png":[["videos.3e9a356c.png",42],42],"./..\\assets\\Images\\images-bw.png":[["images-bw.1af5a4f4.png",41],41],"./..\\assets\\Images\\images.png":[["images.f004ebc2.png",44],44],"./..\\assets\\Images\\animations-bw.png":[["animations-bw.51a68844.png",45],45],"./..\\assets\\Images\\animations.png":[["animations.e5137240.png",46],46],"_css_loader":38}],11:[function(require,module,exports) {
-'use strict';
+},{"./..\\assets\\background-texture.jpg":[["background-texture.1c258b3f.jpg",25],25],"./..\\assets\\Images\\design.png":[["design.022f4afc.png",26],26],"_css_loader":24}],2:[function(require,module,exports) {
+"use strict";
 
-require('./index.scss');
+require("./index.scss");
+
+var content = {
+    videos: ["https://www.youtube.com/watch?v=SR8GqnVY19U&t=340s", "https://www.youtube.com/watch?v=g-2_0FOzMYg", "https://www.youtube.com/watch?v=ikzaG1VwTIA", "https://www.youtube.com/watch?v=RTjOIo_oMTk", "https://www.youtube.com/watch?v=fwb3RmJ7k_A", "https://www.youtube.com/watch?v=ratolK-yxPQ"],
+    animations: ["https://dribbble.com/shots/4503757-Sunnie-Sleeping", "https://dribbble.com/shots/4309821-Fund-A-Life", "https://dribbble.com/shots/4503754-Morphing-Animation", "https://dribbble.com/shots/4502461-Mortimer-the-Molar", "https://cdn.dribbble.com/users/1785918/screenshots/4502482/brio-bumper-v3.gif", "https://dribbble.com/shots/4502478-Bathtime"],
+    images: ["https://farm2.staticflickr.com/1636/24715328805_70ea7986fe_t.jpg", "https://www.flickr.com/photos/138055542@N06/24710712966/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/24347644559/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/39762921640/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/41571122781/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/25676667347/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/40547692981/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/40547693611/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/39139611380/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/40055136775/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/40055136755/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/41348901611/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/25676667527/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/40504902412/in/album-72157690055469520/", "https://www.flickr.com/photos/138055542@N06/26700934287/in/album-72157690055469520/"]
+};
+
+function youtubeIframe(url) {
+    var embedUrl = url.replace("watch?v=", "embed/");
+    return '<iframe width="560" height="315" src="' + embedUrl + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
 
 $(function () {
+    content.videos.forEach(function (url) {
+        $('.content--videos .iframe-holder').append(youtubeIframe(url));
+    });
+    content.animations.forEach(function (url) {
+        $('.content--animations').append($('<img/>').attr('src', url));
+    });
+    content.images.forEach(function (url) {
+        $('.content--images').append($('<img/>').attr('src', url));
+    });
     $('.link').click(function () {
         var content = $(this).data('content');
         if (content) {
             $('.content').removeClass('visible');
-            $('.content--' + content).addClass('visible');
+            $(".content--" + content).addClass('visible');
 
             // Manage icon color based on which content type is selected:
-            var colorIcons = $(this).data('color-icons').split(',');
-            $('.icon').removeClass('icon--color');
+            var colorIcons = $(this).data('color-icons') && $(this).data('color-icons').split(',') || [];
+            $(".icon").removeClass('icon--color');
             for (var i = 0; i < colorIcons.length; i++) {
                 var iconName = colorIcons[i];
-                $('.icon.icon--' + iconName).addClass('icon--color');
+                $(".icon.icon--" + iconName).addClass('icon--color');
             }
         }
     });
 });
-},{"./index.scss":27}],58:[function(require,module,exports) {
+},{"./index.scss":23}],66:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -196,7 +216,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59309' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '58644' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -335,5 +355,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[58,11])
+},{}]},{},[66,2])
 //# sourceMappingURL=/homepage.2d6272fe.map
